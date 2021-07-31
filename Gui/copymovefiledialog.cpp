17e58d7192f19d06773483ce8e32d889785a3a64
brok1n <brok1n@outlook.com>
2021-07-31 21:59:15 +0800

    copy window process exists file ui

--- Gui/copymovefiledialog.cpp
+++ Gui/copymovefiledialog.cpp
@@ -5,6 +5,9 @@
 #include <QTimer>
 #include <QDebug>
 #include <QLocale>
+#include <QUrl>
+#include <QDesktopServices>
+#include <QThread>
 
 CopyMoveFileDialog::CopyMoveFileDialog(QList<QString> files, QString targetPath, bool isMove, QWidget *parent)
     : QDialog(parent)
@@ -14,22 +17,17 @@ CopyMoveFileDialog::CopyMoveFileDialog(QList<QString> files, QString targetPath,
     , mIsMove(isMove)
     , mFileNumber(files.size())
     , mFileSumSize(getFileCountSize(mFiles))
-    , mFileRemainSumSize(mFileSumSize)
+    , mFileProcessSize(0)
+    , mCurrentFilePath("")
+    , mWaitUserSelection(false)
 {
     ui->setupUi(this);
 
     setWindowFlags(windowFlags()|Qt::WindowMinimizeButtonHint|Qt::WindowCloseButtonHint);
 
-    if(mIsMove)
-    {
-        setWindowTitle("正在移动...");
-    }
-    else
-    {
-        setWindowTitle("正在复制...");
-    }
+    flushWindowTitle();
 
-    this->ui->fileCountLabel->setText(QString::number(mFileNumber));
+    this->setProcessFileNumber(mFileNumber);
 
 //    if(mFileNumber > 0)
 //    {
@@ -44,18 +42,17 @@ CopyMoveFileDialog::CopyMoveFileDialog(QList<QString> files, QString targetPath,
 
 
     QFileInfo targetPathInfo(mTargetPath);
-    this->ui->targetPathBtn->setText(targetPathInfo.fileName());
+    this->setTargetDir(targetPathInfo.fileName());
 
     this->ui->progressBar->setMinimum(0);
     this->ui->progressBar->setMaximum(100);
     this->ui->progressBar->setValue(0);
 
-
-
 }
 
 CopyMoveFileDialog::~CopyMoveFileDialog()
 {
+    qDebug() << "释放复制/移动窗口...";
     delete ui;
 }
 
@@ -83,23 +80,163 @@ QString CopyMoveFileDialog::localFileSize(quint64 size)
     return valueText;
 }
 
+void CopyMoveFileDialog::setSourceDir(QString dirName)
+{
+    if(dirName.length() > 15)
+    {
+        dirName = dirName.mid(0, 15);
+        dirName.append("...");
+    }
+    this->ui->sourcePathBtn->setText(dirName);
+    this->ui->sourcePathBtn2->setText(dirName);
+}
+
+void CopyMoveFileDialog::setTargetDir(QString dirName)
+{
+    if(dirName.length() > 15)
+    {
+        dirName = dirName.mid(0, 15);
+        dirName.append("...");
+    }
+    this->ui->targetPathBtn->setText(dirName);
+    this->ui->targetPathBtn2->setText(dirName);
+}
+
+void CopyMoveFileDialog::setProcessFileNumber(int number)
+{
+    this->ui->fileCountLabel->setText(QString::number(number));
+    this->ui->fileCountLabel2->setText(QString::number(number));
+}
+
+void CopyMoveFileDialog::flushWindowTitle()
+{
+    if(mWaitUserSelection)
+    {
+        this->ui->stackedWidget->setCurrentIndex(1);
+        this->setWindowTitle("替换或跳过文件");
+    }
+    else
+    {
+        this->ui->stackedWidget->setCurrentIndex(0);
+        if(mIsMove)
+        {
+            setWindowTitle("正在移动...");
+        }
+        else
+        {
+            setWindowTitle("正在复制...");
+        }
+    }
+
+}
+
 void CopyMoveFileDialog::startProcess()
 {
     qDebug() << "开始处理文件";
+    int process = 0;
     for (int i = 0; i < mFileNumber; i++)
     {
-        QString filePath = mFiles.at(i);
-        QFileInfo fileInfo(filePath);
+        mCurrentFilePath = mFiles.at(i);
+        QFileInfo fileInfo(mCurrentFilePath);
         QFileInfo fileDirInfo(fileInfo.absolutePath());
-        this->ui->sourcePathBtn->setText(fileDirInfo.fileName());
+        this->setSourceDir(fileDirInfo.fileName());
         this->ui->currentFileNameLabel->setText(fileInfo.fileName());
+        this->ui->remainItemsLabel->setText(QString("%1 (%2)").arg(mFileNumber - i).arg(localFileSize(mFileSumSize - mFileProcessSize)));
         QApplication::processEvents();
-        this->ui->remainItemsLabel->setText(QString("%1 (%2)").arg(mFileNumber - i).arg(localFileSize(mFileRemainSumSize)));
 
         //目标
         QFileInfo target(mTargetPath + "/" + fileInfo.fileName());
 
+        if(target.exists())
+        {
+            qDebug() << "文件已存在:" << mCurrentFilePath;
+            mWaitUserSelection = true;
+            this->ui->existsFileNameLabel->setText(fileInfo.fileName());
+            flushWindowTitle();
+            while (mWaitUserSelection) {
+                QApplication::processEvents();
+                QThread::msleep(1);
+            }
+            flushWindowTitle();
+            this->ui->stackedWidget->setCurrentIndex(0);
+        }
+
+        QFile out(target.absoluteFilePath());
+        out.open(QIODevice::WriteOnly);
 
 
+        QFile input(mCurrentFilePath);
+        input.open(QIODevice::ReadOnly);
+
+
+        while (!input.atEnd()) {
+            //一次度128Kb
+            QByteArray data = input.read(131072);
+            out.write(data);
+            out.flush();
+            mFileProcessSize += data.length();
+            this->ui->remainItemsLabel->setText(QString("%1 (%2)").arg(mFileNumber - i).arg(localFileSize(mFileSumSize - mFileProcessSize)));
+
+            process = (int)((float)mFileProcessSize / (float)mFileSumSize * 100);
+            this->ui->progressBar->setValue(process);
+            qDebug() << "process:" << process;
+            QApplication::processEvents();
+        }
+        input.close();
+        out.close();
+        QApplication::processEvents();
+    }
+    this->ui->progressBar->setValue(100);
+    QApplication::processEvents();
+    this->ui->remainTimeLabel->setText("处理完成");
+    this->ui->remainItemsLabel->setText("处理完成");
+    QApplication::processEvents();
+    QTimer::singleShot(1000, this, SLOT(onProcessFinished()));
+
+}
+
+void CopyMoveFileDialog::onProcessFinished()
+{
+    QApplication::processEvents();
+    this->close();
+    this->deleteLater();
+}
+
+void CopyMoveFileDialog::on_sourcePathBtn_clicked()
+{
+    if(!mCurrentFilePath.isEmpty())
+    {
+        QFileInfo currentFileInfo(mCurrentFilePath);
+        QUrl _url = QUrl::fromLocalFile(currentFileInfo.absolutePath());
+        QDesktopServices::openUrl(_url);
     }
 }
+
+
+void CopyMoveFileDialog::on_targetPathBtn_clicked()
+{
+    if(!mTargetPath.isEmpty())
+    {
+        QUrl _url = QUrl::fromLocalFile(mTargetPath);
+        QDesktopServices::openUrl(_url);
+    }
+}
+
+
+void CopyMoveFileDialog::on_replaceBtn_clicked()
+{
+    mWaitUserSelection = false;
+}
+
+
+void CopyMoveFileDialog::on_ignoreBtn_clicked()
+{
+
+}
+
+
+void CopyMoveFileDialog::on_diffBtn_clicked()
+{
+
+}
+
--- Gui/copymovefiledialog.h
+++ Gui/copymovefiledialog.h
@@ -15,15 +15,42 @@ public:
     explicit CopyMoveFileDialog(QList<QString> files, QString targetPath, bool isMove = false, QWidget *parent = nullptr);
     ~CopyMoveFileDialog();
 
+    //开始复制或移动
     void start();
 
+    //获取传入的列表文件总文件大小
     quint64 getFileCountSize(QList<QString>);
 
+    //将文件大小(byte)转换成本地显示格式 Kb  Mb  Gb ...
     QString localFileSize(quint64 size);
 
+    //设置界面显示的源文件所在文件夹
+    void setSourceDir(QString);
+    //设置界面显示的目标文件夹
+    void setTargetDir(QString);
+    //设置复制文件个数
+    void setProcessFileNumber(int number);
+
+    //刷新标题
+    void flushWindowTitle();
+
 private slots:
+    //开始处理，为了不阻塞start方法，这里使用了个定时器启动的新方法处理
     void startProcess();
 
+    //处理完毕
+    void onProcessFinished();
+
+    //源文件所在目录按钮被点击
+    void on_sourcePathBtn_clicked();
+    //目标目录按钮被点击
+    void on_targetPathBtn_clicked();
+
+    void on_replaceBtn_clicked();
+
+    void on_ignoreBtn_clicked();
+
+    void on_diffBtn_clicked();
 
 private:
     Ui::CopyMoveFileDialog *ui;
@@ -32,7 +59,9 @@ private:
     bool mIsMove;   //是否为移动操作
     int mFileNumber; //文件个数
     quint64 mFileSumSize;   //文件总大小
-    quint64 mFileRemainSumSize; //剩余总大小
+    quint64 mFileProcessSize; //已处理大小
+    QString mCurrentFilePath;   //当前在处理的文件路径
+    bool mWaitUserSelection;    //是否等待用户选择
 };
 
 #endif // COPYMOVEFILEDIALOG_H
